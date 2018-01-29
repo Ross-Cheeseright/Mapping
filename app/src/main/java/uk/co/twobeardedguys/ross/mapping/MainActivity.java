2de153420c711cca24f2a9bcd7e950a0f53ca9d6
Ross Cheeseright <r.cheeseright@gmail.com>
2018-01-29 12:18:50 +0000

    Adding map stuff

--- app/src/main/java/uk/co/twobeardedguys/ross/mapping/MainActivity.java
+++ app/src/main/java/uk/co/twobeardedguys/ross/mapping/MainActivity.java
@@ -1,6 +1,8 @@
 
 package uk.co.twobeardedguys.ross.mapping;
 
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,13 +10,16 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Polyline;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements OnClickListener
 {
 
     MapView mv;
@@ -31,10 +36,21 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
 
+        Button button = (Button)findViewById(R.id.btn1);
+        button.setOnClickListener(this);
         mv = (MapView)findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
         mv.getController().setCenter(new GeoPoint(51.0732,-1.7963));
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText et1 = (EditText)findViewById(R.id.et1);
+        EditText et2 = (EditText)findViewById(R.id.et2);
+        double lat = Double.parseDouble(et1.getText().toString());
+        double lon = Double.parseDouble(et2.getText().toString());
+        mv.getController().setCenter(new GeoPoint(lat,lon));
     }
 }
